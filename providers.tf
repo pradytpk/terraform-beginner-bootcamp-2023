df1c617da75e671f77f80a804e0289b17f79a89f
Pradeep Kumar <pradytpk@gmail.com>
2023-09-29 09:47:07 +0000

    #24 configuration drift updates

--- providers.tf
+++ providers.tf
@@ -14,10 +14,6 @@ terraform {
     }
   }
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "5.16.2"
@@ -25,8 +21,4 @@ terraform {
   }
 }
 provider "aws" {
-}
-
-provider "random" {
-  # Configuration options
 }
