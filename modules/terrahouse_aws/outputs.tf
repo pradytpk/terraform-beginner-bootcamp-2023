e0699d77efb12ad55732c3395cc9f212e2a1287c
Pradeep Kumar <pradytpk@gmail.com>
2023-10-14 00:38:41 +0000

    Terratown Test #48 code

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -1,12 +1,14 @@
 output "bucket_name" {
-  value = aws_s3_bucket.website_bucket.bucket
+  description = "Bucket name for our static website hosting"
+  value = module.terrahouse_aws.bucket_name
 }
 
-output "website_endpoint" {
-  value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+output "s3_website_endpoint" {
+  description = "S3 Static Website hosting endpoint"
+  value = module.terrahouse_aws.website_endpoint
 }
 
 output "cloudfront_url" {
-  value = aws_cloudfront_distribution.s3_distribution.domain_name
+  description = "The CloudFront Distribution Domain Name"
+  value = module.terrahouse_aws.cloudfront_url
 }
-
