0c1c7724f6565d898d7e07920b6f0ff96e5e843d
Pradeep Kumar <pradytpk@gmail.com>
2023-10-14 00:04:07 +0000

    #43 Implementation of terraform provider

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ provider "terratowns" {
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
   }
+  
 # module "terrahouse_aws" {
 #   source = "./modules/terrahouse_aws"
 #   user_uuid = var.user_uuid
